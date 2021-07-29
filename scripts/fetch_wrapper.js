20bb04d6941fc68f2ae9d4a1f8a02d28dddf88e9
Dylan <2894220@gmail.com>
2021-07-29 16:19:16 +0800

    find nertc-sdk libraries automatically
    Signed-off-by: Dylan <2894220@gmail.com>

--- scripts/fetch_wrapper.js
+++ scripts/fetch_wrapper.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const download = require('download')
-const tar = require('tar')
 const fs = require('fs')
-const extract = require('extract-zip')
 const { logger } = require('just-task')
 
 module.exports = ({
@@ -17,30 +15,67 @@ module.exports = ({
     if (fs.existsSync(extractPath)) {
       fs.rmdirSync(extractPath, { recursive: true })
     }
-    download(fetchUrl, extractPath, { extract: true, strip: 1 }).then(() => {
+    const temporaryPath = path.join(extractPath, 'temporary')
+    download(fetchUrl, platform === 'win32' ? temporaryPath : extractPath, { extract: true, strip: 1 }).then(() => {
       if (platform === 'win32') {
-        let copyArch
-        if (arch === 'ia32') {
-          copyArch = 'x86'
-        } else if (arch === 'x64') {
-          copyArch = 'x64'
-        } else {
-          reject(new Error('Unsupported arch.'))
+        let binaryDirectory = ''
+        let libraryDirectory = ''
+        let headersDirectory = ''
+        function readDirectories(rootDir, arch) {
+          const dirs = fs.readdirSync(rootDir)
+          dirs.map(dir => {
+            const joinedPath = path.join(rootDir, dir)
+            const stats = fs.lstatSync(joinedPath)
+            if (stats.isDirectory()) {
+              readDirectories(joinedPath, arch)
+            } else {
+              let matchBin
+              let matchLib
+              const matchInclude = new RegExp(/nertc.+\.h/g)
+              if (arch === 'ia32') {
+                matchBin = new RegExp(/x86.+nertc_sdk\.dll/g)
+                matchLib = new RegExp(/x86.+nertc_sdk\.lib/g)
+              } else if (arch === 'x64') {
+                matchBin = new RegExp(/x64.+nertc_sdk\.dll/g)
+                matchLib = new RegExp(/x64.+nertc_sdk\.lib/g)
+              }
+              if (matchBin.test(joinedPath) && binaryDirectory === '') {
+                binaryDirectory = rootDir
+              }
+              if (matchLib.test(joinedPath) && libraryDirectory === '') {
+                libraryDirectory = rootDir
+              }
+              if (matchInclude.test(joinedPath) && headersDirectory === '') {
+                headersDirectory = rootDir
+              }
+            }
+          })
         }
-        const dllSrcPath = path.join(extractPath, `dll/${copyArch}`)
-        const libSrcPath = path.join(extractPath, `lib/${copyArch}`)
-        const dlls = fs.readdirSync(dllSrcPath)
-        logger.info(dlls)
-        dlls.map((dll) => {
-          logger.info(`Copy file from ${dll} to ${extractPath}/dll`)
-          fs.copyFileSync(path.join(dllSrcPath, dll), path.join(extractPath, `dll/${dll}`))
-        })
-        const libs = fs.readdirSync(libSrcPath)
-        logger.info(libs)
-        libs.map((lib) => {
-          logger.info(`Copy file from ${lib} to ${extractPath}/lib`)
-          fs.copyFileSync(path.join(libSrcPath, lib), path.join(extractPath, `lib/${lib}`))
-        })
+
+        // find bin/lib/api directory automatically
+        readDirectories(temporaryPath, arch)
+
+        function copyFiles(src, dst, suffix) {
+          if (!fs.existsSync(dst)) {
+            fs.mkdirSync(dst)
+          }
+          const list = fs.readdirSync(src)
+          list.map(file => {
+            if (file.indexOf(suffix) !== -1) {
+              logger.info(`[fetch] Copy file from ${path.join(src, file)} to ${dst}`)
+              fs.copyFileSync(path.join(src, file), path.join(dst, file))
+            }
+          })
+        }
+
+        // copy files from temporary to nertc_sdk
+        copyFiles(binaryDirectory, path.join(extractPath, 'dll'), '.dll')
+        copyFiles(libraryDirectory, path.join(extractPath, 'lib'), '.lib')
+        copyFiles(headersDirectory, path.join(extractPath, 'api'), '.h')
+
+        // remove temporary directory
+        fs.rmdirSync(temporaryPath, { recursive: true })
+
       } else if (platform === 'darwin') {
       } else {
         return reject(new Error('Unsupported platform.'))
