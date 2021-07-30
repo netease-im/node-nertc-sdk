033d7b14b22bea399465b0c5851ed83cb5e6fe1b
Dylan <2894220@gmail.com>
2021-07-30 17:54:46 +0800

    copy .framework from SDK folder automatically
    Signed-off-by: Dylan <2894220@gmail.com>

--- scripts/fetch_wrapper.js
+++ scripts/fetch_wrapper.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const download = require('download')
 const fs = require('fs')
+const fsExtra = require('fs-extra')
 const { logger } = require('just-task')
 
 module.exports = ({
@@ -16,7 +17,7 @@ module.exports = ({
       fs.rmdirSync(extractPath, { recursive: true })
     }
     const temporaryPath = path.join(extractPath, 'temporary')
-    download(fetchUrl, platform === 'win32' ? temporaryPath : extractPath, { extract: true, strip: 1 }).then(() => {
+    download(fetchUrl, temporaryPath, { extract: true, strip: 1 }).then(() => {
       if (platform === 'win32') {
         let binaryDirectory = ''
         let libraryDirectory = ''
@@ -51,10 +52,8 @@ module.exports = ({
             }
           })
         }
-
         // find bin/lib/api directory automatically
         readDirectories(temporaryPath, arch)
-
         function copyFiles(src, dst, suffix) {
           if (!fs.existsSync(dst)) {
             fs.mkdirSync(dst)
@@ -67,19 +66,44 @@ module.exports = ({
             }
           })
         }
-
         // copy files from temporary to nertc_sdk
         copyFiles(binaryDirectory, path.join(extractPath, 'dll'), '.dll')
         copyFiles(libraryDirectory, path.join(extractPath, 'lib'), '.lib')
         copyFiles(headersDirectory, path.join(extractPath, 'api'), '.h')
-
-        // remove temporary directory
-        fs.rmdirSync(temporaryPath, { recursive: true })
-
       } else if (platform === 'darwin') {
+        let frameworkDirectory = ''
+        const marchFramework = new RegExp(/.+\.framework$/g)
+        function readDirectory(rootDir, arch) {
+          const dirs = fs.readdirSync(rootDir)
+          dirs.map(dir => {
+            const newPath = path.join(rootDir, dir)
+            const stats = fs.lstatSync(newPath)
+            if (marchFramework.test(dir) &&
+                newPath.indexOf('demo/') === -1 &&
+                newPath.indexOf('.app/Contents') === -1 &&
+                frameworkDirectory === '') {
+              frameworkDirectory = rootDir
+            } else if (stats.isDirectory()) {
+              readDirectory(path.join(rootDir, dir), arch)
+            }
+          })
+        }
+        readDirectory(temporaryPath, arch)
+        const list = fs.readdirSync(frameworkDirectory)
+        logger.info('==== ', frameworkDirectory)
+        list.map(framework => {
+          logger.info('- ', framework)
+          if (framework.indexOf('.framework') !== -1) {
+            const copied = path.join(frameworkDirectory, framework)
+            logger.info(`[fetch] copy file: ${copied}`)
+            fsExtra.copySync(copied, path.join(extractPath, framework))
+          }
+        })
       } else {
         return reject(new Error('Unsupported platform.'))
       }
+      // remove temporary directory
+      fs.rmdirSync(temporaryPath, { recursive: true })
       resolve()
     }).catch(err => {
       console.log(err);
