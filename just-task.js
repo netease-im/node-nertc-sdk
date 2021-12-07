9909fe90bcc3baea4ce87a5e19b20c165190cc5d
liyongqiang01 <liyongqiang01@corp.netease.com>
2021-12-07 21:18:34 +0800

    chore: upd url

--- just-task.js
+++ just-task.js
@@ -19,7 +19,7 @@ const includePath = 'nertc_sdk'
 const tempPath = 'temporary'
 const packageMeta = require(path.join(__dirname, 'package.json'))
 const nativeWinUrl = `http://yx-web.nos.netease.com/package/1638867034114/NERTC_Windows_SDK_V4.1.114.zip`
-const nativeMacUrl = `http://yx-web.nos.netease.com/package/1638867022121/NERTC_MacOS_SDK_V4.1.114.zip`
+const nativeMacUrl = `http://yx-web.nos.netease.com/package/1638882935/NERTC_MacOS_SDK_V4.1.114.zip`
 
 task('fetch-wrapper', () => {
   const platform = argv().target_platform
