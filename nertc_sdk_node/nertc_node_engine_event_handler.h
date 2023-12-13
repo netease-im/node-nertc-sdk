f96e4b41df16656a37d961e01662bd0b0071fbae
liyongqiang01 <liyongqiang01@corp.netease.com>
2023-12-13 14:03:35 +0800

    bugfix

--- nertc_sdk_node/nertc_node_engine_event_handler.h
+++ nertc_sdk_node/nertc_node_engine_event_handler.h
@@ -127,7 +127,6 @@ public:
     virtual void onFirstVideoFrameRender(nertc::NERtcVideoStreamType type, nertc::uid_t uid, uint32_t width, uint32_t height, uint64_t elapsed) override;
     virtual void onLabFeatureCallback(const char* key, const char* param) override;
     virtual void onScreenCaptureSourceDataUpdate(nertc::NERtcScreenCaptureSourceData data) override;
-    virtual void (nertc::NERtcVideoStreamType videoStreamType, nertc::NERtcLocalVideoWatermarkState state) override;
     
 public:
     void onPullExternalAudioFrame(Napi::FunctionReference&& function, const std::shared_ptr<unsigned char>& data, uint32_t length);
